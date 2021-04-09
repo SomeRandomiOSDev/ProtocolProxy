fa335ab842f80a1190638a86792fd73068785251
Joe Newton <>
2021-04-09 16:14:47 -0400

    Updated XCFramework script and added a scheme for building an XCFramework through Xcode

--- scripts/xcframework.sh
+++ scripts/xcframework.sh
@@ -13,6 +13,12 @@ while [[ $# -gt 0 ]]; do
         shift # <output_dir>
         ;;
 
+        -configuration)
+        CONFIGURATION="$2"
+        shift # -configuration
+        shift # <configuration>
+        ;;
+
         *)
         echo "Unknown argument: $1"
         echo "./xcframework.sh [-output <output_xcframework>]"]
@@ -21,13 +27,17 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [ -z ${OUTPUT_DIR+x} ]; then
-    OUTPUT_DIR="$(dirname $(realpath $0))/build/ProtocolProxy.xcframework"
+    OUTPUT_DIR="$(dirname "$(realpath "$0")")/build/ProtocolProxy.xcframework"
+fi
+
+if [ -z ${CONFIGURATION+x} ]; then
+    CONFIGURATION="Release"
 fi
 
 # Create Temporary Directory
 
 TMPDIR=`mktemp -d /tmp/.protocolproxy.xcframework.build.XXXXXX`
-cd "$(dirname $(dirname $(realpath $0)))"
+cd "$(dirname "$(dirname "$(realpath "$0")")")"
 
 check_result() {
     if [ $1 -ne 0 ]; then
@@ -37,35 +47,35 @@ check_result() {
 }
 
 # Build iOS
-xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy" -destination "generic/platform=iOS" -archivePath "${TMPDIR}/iphoneos.xcarchive" -configuration Release SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64 arm64e" archive
+xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy" -destination "generic/platform=iOS" -archivePath "${TMPDIR}/iphoneos.xcarchive" -configuration ${CONFIGURATION} SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="armv7 armv7s arm64 arm64e" archive
 check_result $?
 
 # Build iOS Simulator
-xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy" -destination "generic/platform=iOS Simulator" -archivePath "${TMPDIR}/iphonesimulator.xcarchive" -configuration Release SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="i386 x86_64 arm64" archive
+xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy" -destination "generic/platform=iOS Simulator" -archivePath "${TMPDIR}/iphonesimulator.xcarchive" -configuration ${CONFIGURATION} SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="i386 x86_64 arm64" archive
 check_result $?
 
 # Build Mac Catalyst
-xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy" -destination "generic/platform=macOS,variant=Mac Catalyst" -archivePath "${TMPDIR}/maccatalyst.xcarchive" -configuration Release SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="x86_64 arm64 arm64e" archive
+xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy" -destination "generic/platform=macOS,variant=Mac Catalyst" -archivePath "${TMPDIR}/maccatalyst.xcarchive" -configuration ${CONFIGURATION} SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="x86_64 arm64 arm64e" archive
 check_result $?
 
 # Build Mac
-xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy macOS" -destination "generic/platform=macOS" -archivePath "${TMPDIR}/macos.xcarchive" -configuration Release SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="x86_64 arm64 arm64e" archive
+xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy macOS" -destination "generic/platform=macOS" -archivePath "${TMPDIR}/macos.xcarchive" -configuration ${CONFIGURATION} SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="x86_64 arm64 arm64e" archive
 check_result $?
 
 # Build tvOS
-xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy tvOS" -destination "generic/platform=tvOS" -archivePath "${TMPDIR}/appletvos.xcarchive" -configuration Release SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="arm64 arm64e" archive
+xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy tvOS" -destination "generic/platform=tvOS" -archivePath "${TMPDIR}/appletvos.xcarchive" -configuration ${CONFIGURATION} SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="arm64 arm64e" archive
 check_result $?
 
 # Build tvOS Simulator
-xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy tvOS" -destination "generic/platform=tvOS Simulator" -archivePath "${TMPDIR}/appletvsimulator.xcarchive" -configuration Release SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="x86_64 arm64" archive
+xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy tvOS" -destination "generic/platform=tvOS Simulator" -archivePath "${TMPDIR}/appletvsimulator.xcarchive" -configuration ${CONFIGURATION} SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="x86_64 arm64" archive
 check_result $?
 
 # Build watchOS
-xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy watchOS" -destination "generic/platform=watchOS" -archivePath "${TMPDIR}/watchos.xcarchive" -configuration Release SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="arm64_32 armv7k" archive
+xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy watchOS" -destination "generic/platform=watchOS" -archivePath "${TMPDIR}/watchos.xcarchive" -configuration ${CONFIGURATION} SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="arm64_32 armv7k" archive
 check_result $?
 
 # Build watchOS Simulator
-xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy watchOS" -destination "generic/platform=watchOS Simulator" -archivePath "${TMPDIR}/watchsimulator.xcarchive" -configuration Release SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="i386 x86_64 arm64" archive
+xcodebuild -project "ProtocolProxy.xcodeproj" -scheme "ProtocolProxy watchOS" -destination "generic/platform=watchOS Simulator" -archivePath "${TMPDIR}/watchsimulator.xcarchive" -configuration ${CONFIGURATION} SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES ONLY_ACTIVE_ARCH=NO ARCHS="i386 x86_64 arm64" archive
 check_result $?
 
 # Make XCFramework
@@ -74,16 +84,23 @@ if [[ -d "${OUTPUT_DIR}" ]]; then
     rm -rf "${OUTPUT_DIR}"
 fi
 
-xcodebuild -create-xcframework                                                                            \
-    -framework "${TMPDIR}/iphoneos.xcarchive/Products/Library/Frameworks/ProtocolProxy.framework"         \
-    -framework "${TMPDIR}/iphonesimulator.xcarchive/Products/Library/Frameworks/ProtocolProxy.framework"  \
-    -framework "${TMPDIR}/maccatalyst.xcarchive/Products/Library/Frameworks/ProtocolProxy.framework"      \
-    -framework "${TMPDIR}/macos.xcarchive/Products/Library/Frameworks/ProtocolProxy.framework"            \
-    -framework "${TMPDIR}/appletvos.xcarchive/Products/Library/Frameworks/ProtocolProxy.framework"        \
-    -framework "${TMPDIR}/appletvsimulator.xcarchive/Products/Library/Frameworks/ProtocolProxy.framework" \
-    -framework "${TMPDIR}/watchos.xcarchive/Products/Library/Frameworks/ProtocolProxy.framework"          \
-    -framework "${TMPDIR}/watchsimulator.xcarchive/Products/Library/Frameworks/ProtocolProxy.framework"   \
-    -output "${OUTPUT_DIR}"
+ARGUMENTS=(-create-xcframework -output "${OUTPUT_DIR}")
+
+for ARCHIVE in ${TMPDIR}/*.xcarchive; do
+    ARGUMENTS=(${ARGUMENTS[@]} -framework "${ARCHIVE}/Products/Library/Frameworks/ProtocolProxy.framework")
+
+    if [[ -d "${ARCHIVE}/dSYMs/ProtocolProxy.framework.dSYM" ]]; then
+        ARGUMENTS=(${ARGUMENTS[@]} -debug-symbols "${ARCHIVE}/dSYMs/ProtocolProxy.framework.dSYM")
+    fi
+
+    if [[ -d "${ARCHIVE}/BCSymbolMaps" ]]; then
+        for SYMBOLMAP in ${ARCHIVE}/BCSymbolMaps/*.bcsymbolmap; do
+            ARGUMENTS=(${ARGUMENTS[@]} -debug-symbols "${SYMBOLMAP}")
+        done
+    fi
+done
+
+xcodebuild "${ARGUMENTS[@]}"
 check_result $?
 
 # Cleanup
